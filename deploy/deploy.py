f088645bfcba0077d38ddea00d25b2afc5e6b94c
Hadar Shavit <shavit.hadar@gmail.com>
2022-09-28 16:53:45 +0200

    Test of deployment

--- deploy/deploy.py
+++ deploy/deploy.py
@@ -25,7 +25,7 @@ class Deploy(ABC):
             f.writelines(workers)
 
     def connect_to_master(self, master):
-        self.client = paramiko.client.SSHClient(self.master)
+        self.client = paramiko.client.SSHClient(master)
 
     def start_system(self):
         self.client.exec_command(f'{self.executable_file_path}')
@@ -38,3 +38,20 @@ class Deploy(ABC):
 class DeploySpark(Deploy):
     def __init__(self):
         super().__init__()
+        self.executable_file_path = '~/scratch/spark-3.3.0-bin-hadoop3/sbin/start-all.sh'
+        self.stop_file_path = '~/scratch/spark-3.3.0-bin-hadoop3/sbin/stop-all.sh'
+        self.workers_file_path = '~/scratch/spark-3.3.0-bin-hadoop3/sbin/workers'
+
+
+if __name__ == '__main__':
+    print("Testing Spark Deployment")
+    d = DeploySpark()
+    nodes = d.reserve_nodes(2, 10)
+    print(f'Reserved nodes {nodes}')
+    d.update_workers(nodes[1:])
+    print('Updates workers file')
+    d.connect_to_master(nodes[0])
+    print('Connected to master')
+    d.start_system()
+    print('Started system')
+
